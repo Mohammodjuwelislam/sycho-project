3754a46d4a166b1764fe6f38d2537e67b5014508
Mohammodjuwelislam <mohammodjuwel61@gmail.com>
2022-12-12 01:45:41 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+(function ($) {
+    "use strict";
+
+   
+	$('.slider-active').slick({
+		dots: true,
+		infinite: true,
+		slidesToShow: 1,
+		speed: 500,
+		autoplay: true,
+        autoplaySpeed: 3000,
+		arrows:false,
+	});
+		
+
+})(jQuery);
